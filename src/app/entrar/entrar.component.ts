0aa42188a6b7206f14efafaa9d5a238634fcced0
Maria Luiza Filinto <92994711+malufilinto@users.noreply.github.com>
2022-04-01 03:12:33 -0300

    Update entrar.component.ts

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { AlertasService } from './../service/alertas.service';
+import { environment } from './../../environments/environment.prod';
 import { Router } from '@angular/router';
-import { environment } from 'src/environments/environment.prod';
-import { UsuarioLogin } from '../model/UsuarioLogin';
-import { AuthService } from '../service/auth.service';
+import { AuthService } from './../service/auth.service';
+import { UserLogin } from './../model/UserLogin';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-entrar',
@@ -11,32 +12,36 @@ import { AuthService } from '../service/auth.service';
 })
 export class EntrarComponent implements OnInit {
 
-  usuarioLogin: UsuarioLogin = new UsuarioLogin()
+  userLogin: UserLogin = new UserLogin()
 
   constructor(
     private auth: AuthService,
-    private router: Router
+    private router: Router,
+    private alertas: AlertasService
   ) { }
 
   ngOnInit() {
-    window.scroll(0, 0)
+    window.scroll(0,0)
+
+
   }
 
-  entrar() {
-    this.auth.entrar(this.usuarioLogin).subscribe({
-      next: (resp: UsuarioLogin) => {
-        this.usuarioLogin = resp
-        environment.token = this.usuarioLogin.token
-        environment.nome = this.usuarioLogin.nome
-        environment.foto = this.usuarioLogin.foto
-        environment.id = this.usuarioLogin.id
-        this.router.navigate(['/inicio'])
-      },
-      error: erro => {
-          if (erro.status == 401) {
-            alert('Usuario ou senha estão incorretos!')
-          }
-        },
-    });
+  entrar(){
+    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin)=>{
+      this.userLogin = resp
+
+      environment.token = this.userLogin.token
+      environment.nome = this.userLogin.nome
+      environment.foto = this.userLogin.foto
+      environment.id = this.userLogin.id
+      environment.tipo = this.userLogin.tipo
+
+      this.router.navigate(['/inicio'])
+    }, erro =>{
+      if(erro.status == 500){
+        this.alertas.showAlertDanger('Usuário ou senha estão incorretos!')
+      }
+    })
   }
+
 }
